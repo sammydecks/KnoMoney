035d7da7d4bd7a9af25f50fca4999af32b823501
Samantha Decker <sdecker@mines.edu>
2025-02-16 16:43:27 -0700

    Init calculator page

--- KnoMoney/app/views.py
+++ KnoMoney/app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def calculator(request):
+    return render(request, "calculator.html")
