7d62978e04a31e17ce536516acb7579e43b8713b
Samantha Decker <sdecker@mines.edu>
2025-02-21 12:12:14 -0700

    input validation on calculator

--- KnoMoney/app/static/scripts/addLoan.js
+++ KnoMoney/app/static/scripts/addLoan.js
@@ -7,17 +7,17 @@ document.getElementById("addLoan").addEventListener("click", function() {
     newLoan.innerHTML = `
         <legend>Loan ${loanCount}</legend>
         <label for="balance">Balance ($): </label>
-        <input type="number" name="balance[]" min="1">
+        <input type="number" name="balance[]" min="0.01" step="0.01" required>
         <label for="interest">Interest Rate (%): </label>
-        <input type="number" name="interest[]" min="1">
+        <input type="number" name="interest[]" min="0.01" max="99.99" step="0.01" required>
         <label for="type">Loan Type:</label>
-        <select name="type[]">
+        <select name="type[]" required>
             <option value="" disabled selected>-- Select --</option>
             <option value="unsubsidized">Unsubsidized</option>
             <option value="subsidized">Subsidized</option>
         </select>
         <label for="received">Date Received:</label>
-        <input type="date" name="received[]">
+        <input type="date" name="received[]" required>
     `;
 
     loanContainer.appendChild(newLoan);
