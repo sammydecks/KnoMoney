624d11943628e9b3d0b757a1ea599db18a37061f
Samantha Decker <sdecker@mines.edu>
2025-02-21 17:22:29 -0700

    calculator bootstrap styling

--- KnoMoney/app/static/scripts/addLoan.js
+++ KnoMoney/app/static/scripts/addLoan.js
@@ -1,23 +1,34 @@
-document.getElementById("addLoan").addEventListener("click", function() {
+document.getElementById("addLoan").addEventListener("click", function () {
     const loanContainer = document.getElementById("loanContainer");
     const loanCount = loanContainer.getElementsByClassName("loanEntry").length + 1;
 
     const newLoan = document.createElement("fieldset");
-    newLoan.classList.add("loanEntry");
+    newLoan.classList.add("loanEntry", "border", "p-2", "mb-3");
+
     newLoan.innerHTML = `
-        <legend>Loan ${loanCount}</legend>
-        <label for="balance">Balance ($): </label>
-        <input type="number" name="balance[]" min="0.01" step="0.01" required>
-        <label for="interest">Interest Rate (%): </label>
-        <input type="number" name="interest[]" min="0.01" max="99.99" step="0.01" required>
-        <label for="type">Loan Type:</label>
-        <select name="type[]" required>
-            <option value="" disabled selected>-- Select --</option>
-            <option value="unsubsidized">Unsubsidized</option>
-            <option value="subsidized">Subsidized</option>
-        </select>
-        <label for="received">Date Received:</label>
-        <input type="date" name="received[]" required>
+        <legend class="lead text-center">Loan ${loanCount}</legend>
+        <div class="row">
+            <div class="col">
+                <label class="form-label">Balance ($):</label>
+                <input type="number" class="form-control" name="balance[]" placeholder="Loan Balance" min="0.01" step="0.01" required>
+            </div>
+            <div class="col">
+                <label class="form-label">Interest Rate (%):</label>
+                <input type="number" class="form-control" name="interest[]" placeholder="Loan Interest Rate" min="0.01" max="99.99" step="0.01" required>
+            </div>
+            <div class="col">
+                <label class="form-label">Loan Type:</label>
+                <select name="type[]" class="form-select" required>
+                    <option value="" disabled selected>-- select --</option>
+                    <option value="unsubsidized">Unsubsidized</option>
+                    <option value="subsidized">Subsidized</option>
+                </select>
+            </div>
+            <div class="col">
+                <label class="form-label">Date Received:</label>
+                <input type="date" class="form-control" name="received[]" required>
+            </div>
+        </div>
     `;
 
     loanContainer.appendChild(newLoan);
