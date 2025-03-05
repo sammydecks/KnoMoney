646ff4100aa4ae7e5b7ff7b4a6458163ed99d86b
Samantha Decker <sdecker@mines.edu>
2025-03-05 09:37:02 -0700

    secure requests

--- KnoMoney/settings.py
+++ KnoMoney/settings.py
@@ -28,6 +28,9 @@ DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ["knomoney.com", "www.knomoney.com"]
 
+SECURE_SSL_REDIRECT = True  # Redirects all HTTP requests to HTTPS
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
