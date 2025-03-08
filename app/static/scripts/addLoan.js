175612c73b4a443530e75d208d665e5529f8424c
Samantha Decker <sdecker@mines.edu>
2025-03-08 14:16:08 -0700

    loan inputs responsive

--- app/static/scripts/addLoan.js
+++ app/static/scripts/addLoan.js
@@ -11,15 +11,15 @@ document.getElementById("addLoan").addEventListener("click", function () {
             <button type="button" class="btn-close" aria-label="Close"></button>
         </div>
         <div class="row">
-            <div class="col">
+            <div class="col-12 col-md">
                 <label class="form-label">Balance ($):</label>
                 <input type="number" class="form-control" name="balance[]" placeholder="Loan Balance" min="0.01" max="100000" step="0.01" required>
             </div>
-            <div class="col">
+            <div class="col-12 col-md">
                 <label class="form-label">Interest Rate (%):</label>
                 <input type="number" class="form-control" name="interest[]" placeholder="Loan Interest Rate" min="0.01" max="99.99" step="0.01" required>
             </div>
-            <div class="col">
+            <div class="col-12 col-md">
                 <label class="form-label">Loan Type:</label>
                 <select name="type[]" class="form-select" required>
                     <option value="" disabled selected>-- select --</option>
@@ -27,7 +27,7 @@ document.getElementById("addLoan").addEventListener("click", function () {
                     <option value="subsidized">Subsidized</option>
                 </select>
             </div>
-            <div class="col">
+            <div class="col-12 col-md">
                 <label class="form-label">Date Received:</label>
                 <input type="date" class="form-control" name="received[]" required>
             </div>
