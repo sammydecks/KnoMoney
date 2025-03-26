fb3cbea6a8d1c1b2d76780427922443e8f7c7752
Samantha Decker <sdecker@mines.edu>
2025-03-26 15:24:23 -0600

    no more share button

--- app/static/scripts/share.js
+++ app/static/scripts/share.js
@@ -1,38 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const shareButton = document.getElementById("shareButton");
-    const popup = document.getElementById("sharePopup");
-    const closePopup = document.getElementById("closePopup");
-
-    shareButton.addEventListener("click", function () {
-        popup.classList.remove("d-none"); // Show popup
-
-        // Log the event via an HTTP request
-        fetch("log-share", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                event: "share_clicked",
-                timestamp: new Date().toISOString()
-            })
-        }).catch(error => console.error("Error logging share:", error));
-    });
-
-    closePopup.addEventListener("click", function () {
-        popup.classList.add("d-none"); // Hide popup when Close is clicked
-    });
-
     // Share via email
     document.getElementById("emailButton").addEventListener("click", function() {
-        let textToShare = document.querySelector("#sharePopup .border").innerText;
+        let textToShare = document.querySelector("#share").innerText;
         let emailBody = encodeURIComponent(textToShare);
         let mailtoLink = `mailto:?subject=Potential Student Loan Savings&body=${emailBody}`;
         this.setAttribute("href", mailtoLink);
     });
 
     document.getElementById("copyButton").addEventListener("click", function() {
-        let textToCopy = document.querySelector("#sharePopup .border").innerText; 
+        let textToCopy = document.querySelector("#share").innerText; 
         navigator.clipboard.writeText(textToCopy).then(() => {
             let message = document.getElementById("copyMessage");
             message.classList.remove("d-none");
