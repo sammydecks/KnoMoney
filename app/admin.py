0d5bc574b13aa38f51f6c3c35a8830af9f7a0611
Mimi Rai <22sumnima.rai@gmail.com>
2025-04-06 14:12:15 -0600

    resolve merge conflict in admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,17 +1,8 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import Referral
-
+from .models import Referral, TestTable
 
 # Register your models here.
 admin.site.register(Referral)
-=======
-from .models import MyModel, TestTable
-
-
-# Register your models here.
-admin.site.register(MyModel)
 
 # local DB table test
 admin.site.register(TestTable)
->>>>>>> 74054050e44e7d605d1bba8c8823d194525daad1
