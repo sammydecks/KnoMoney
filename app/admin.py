74054050e44e7d605d1bba8c8823d194525daad1
Samantha Decker <sdecker@mines.edu>
2025-04-06 13:47:39 -0600

    local db setup

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import MyModel
+from .models import MyModel, TestTable
 
 
 # Register your models here.
 admin.site.register(MyModel)
+
+# local DB table test
+admin.site.register(TestTable)
--- app/models.py
+++ app/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class MyModel(models.Model):
     name = models.CharField(max_length=255)
+
+class TestTable(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
